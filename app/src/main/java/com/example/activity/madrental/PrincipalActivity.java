0502d4c22d3b993661828c1748abccaa7141b63e
Camille GUERIN <camille.guerin@ynov.com>
2018-12-07 12:40:48 +0100

    Création de la view recherche

--- app/src/main/java/com/example/activity/madrental/PrincipalActivity.java
+++ app/src/main/java/com/example/activity/madrental/PrincipalActivity.java
@@ -1,7 +1,9 @@
 package com.example.activity.madrental;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +23,8 @@ public class PrincipalActivity extends AppCompatActivity {
         // affichage de la page détail :
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
+
+
     }
 }
