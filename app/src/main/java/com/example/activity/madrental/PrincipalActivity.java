a298c0e321885a4285be5b5d263e3cf978d58ddd
Camille GUERIN <camille.guerin@ynov.com>
2018-12-07 10:37:46 +0100

    Changement de la police

--- app/src/main/java/com/example/activity/madrental/PrincipalActivity.java
+++ app/src/main/java/com/example/activity/madrental/PrincipalActivity.java
@@ -1,13 +1,29 @@
 package com.example.activity.madrental;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    //Constantes :
+    private static final String CHEMIN_FONT = "font/Hansief.otf";
+
+
+    //Vues
+    private TextView textViewLegende = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        //Police de la page principal
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), CHEMIN_FONT);
+        textViewLegende.setTypeface(typeface);
+
     }
 }
